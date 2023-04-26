9fc595bb162e2fe716e8d32a08cef7fe0f599062
isaac.rudasingwa <rudasaacv@gmail.com>
2023-04-26 16:41:02 +0200

    let's connect with client side user

---
+++ Javascript/script1.js
@@ -0,0 +1,35 @@
+const eventsList = document.getElementById('events-list');
+
+function displayEvents(events) {
+    // clear existing events from the list 
+    eventsList.innerHTML = '';
+
+    // add each event to the list as a new <li> element
+    events.forEach(event => {
+        const li = document.createElement('li');
+        li.textContent = '${event.name} (${event.date}) - ${event.location}';
+        eventsList.appendChild(li);
+    });
+}
+
+function loadEvents() {
+    //make a GET request to the API endpoint using AJAX
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://127.0.0.1:8080/api/events?ordering=datePlace');
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            const events = JSON.parse(xhr.response);
+            // update the events list with the new data
+            displayEvents(events);
+        } else {
+            console.error('Request failed. Status code: ${xhr.status}');
+        }
+    };
+    xhr.send();
+}
+
+// load the events when the page loads
+window.addEventListener('load', loadEvents);
+
+// reload the events every 10 seconds
+setInterval(loadEvents, 10000);
